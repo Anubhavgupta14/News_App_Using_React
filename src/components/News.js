e8e20ff795d7fc371290c43f853f55d5d8dc0e52
Anubhavgupta14 <anubhavgu2002@gmail.com>
2022-11-28 02:36:42 +0530

    Update

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsComponents from './NewsComponents'
 export class News extends Component {
   render() {
     return (
-      <div>
-        <NewsComponents/>
-        <NewsComponents/>
-        <NewsComponents/>
-        <NewsComponents/>
-        <NewsComponents/>
+      <div className='container my-3'>
+        <h2>Top Headlines</h2>
+        <div className='row'>
+            <div className='col-md-4'>
+            <NewsComponents title="Title" description="mydes"/>
+            </div>
+            <div className='col-md-4'>
+            <NewsComponents/>
+            </div>
+            <div className='col-md-4'>
+            <NewsComponents/>
+            </div>
+        </div>
       </div>
     )
   }
